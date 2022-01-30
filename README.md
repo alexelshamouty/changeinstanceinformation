6c21a072a75114888a3384686453e982be9573ca
Alex Elshamouty <aelshamouty@gmail.com>
2022-01-30 11:12:15 +0100

    Addint the script to the repo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # changeinstanceinformation
 Manipulating nova's instance information in the DB after properties change in the flavor
+
+
+In my infrastructure we are saperating infrastructure based on certain criteria
+At some point we wanted to change this criteria. The flavor is easy to change.
+But the instance information can not be changed through the CLi. This scripts handles the needed changes in both the request_specs table and the instance_extras flavor.
+
+No idea why nova is saving the information in two different places
---
+++ change_vm_to_shared.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# Requires python 3, pymysql
+# Login to vault first
+# vault login -method=ldap username=<username>
+
+import copy
+import json
+import subprocess
+import sys
+
+import pymysql
+from pprint import pprint
+
+
+def fix_stuff(host, instance):
+    with pymysql.connect(
+        host=host,
+        user=credentials["data"]["user"],
+        password=credentials["data"]["password"],
+        db="nova",
+    ) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                f'select flavor from instance_extra where instance_uuid="{instance}"'
+            )
+            parsed_info = json.loads(cur.fetchone()[0])
+            print("Current info in the instance_extra table")
+            pprint(parsed_info)
+            if (
+                parsed_info["cur"]["nova_object.data"]["extra_specs"]["hypervisor"]
+                != "shared"
+            ):
+                parsed_info["cur"]["nova_object.data"]["extra_specs"][
+                    "hypervisor"
+                ] = "shared"
+                new_info = json.dumps(parsed_info)
+                print(f"Updating instance {instance}")
+                cur.execute(
+                    f"update instance_extra set flavor='{new_info}' where instance_uuid=\"{instance}\""
+                )
+                print(cur._last_executed)
+                conn.commit()
+    print("Checking if we need to fix the request specs")
+    with pymysql.connect(
+        host=host,
+        user=credentials["data"]["user"],
+        password=credentials["data"]["password"],
+        db="novaapi",
+    ) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                f'select spec from request_specs where instance_uuid="{instance}"'
+            )
+            original_specs = json.loads(cur.fetchone()[0])
+            print("Current info in the instance_extra table")
+            pprint(original_specs)
+            if (
+                original_specs["nova_object.data"]["flavor"]["nova_object.data"][
+                    "extra_specs"
+                ]["hypervisor"]
+                != "shared"
+            ):
+                original_specs["nova_object.data"]["flavor"]["nova_object.data"][
+                    "extra_specs"
+                ]["hypervisor"] = "shared"
+                new_info = json.dumps(original_specs)
+                print(f"Updating instance {instance}'s extra specs")
+                cur.execute(
+                    f"update request_specs set spec='{new_info}' where instance_uuid=\"{instance}\""
+                )
+                print(cur._last_executed)
+                conn.commit()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(f"Usage {sys.argv[0]} mysqlhost instasnce_uuid")
+        exit(-1)
+
+    credentials = json.loads(
+        subprocess.check_output(
+            ["vault", "read", "-format", "json", "secret/mysql/nova"]
+        )
+    )
+
+    fix_stuff(sys.argv[1], sys.argv[2])
